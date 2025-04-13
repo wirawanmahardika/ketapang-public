790b7cf6841de58329473179bcde1ad5fdba9f9b
wirawanmahardika <wiraprogrammer@gmail.com>
2025-04-13 11:51:13 +0800

    chore: menyelesaikan berita

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
